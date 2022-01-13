cd89e06a30dd734dbbc6d30d8f76f17cdf4f8680
Mjoel4708 <mjoel4708@gmail.com>
2022-01-13 14:34:33 +0100

    Flutterwave Controller

--- src/Storefront/Controller/FlutterwaveController.php
+++ src/Storefront/Controller/FlutterwaveController.php
@@ -2,11 +2,29 @@
 
 namespace FlutterwavePay\Storefront\Contoller;
 
+use Shopware\Core\Checkout\Order\OrderEntity;
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
 
 class FlutterwaveController
 {
-    function getTransactionData()
-    {
-        
+    function getTransactionData(
+        OrderEntity $order,
+        SalesChannelContext $salesChannelContext,
+        string $returnUrl,
+
+    ): array {
+        $shopwarePaymentMethod = $salesChannelContext->getPaymentMethod()->getId();
+        $salesChannelId = $salesChannelContext->getSalesChannel()->getId();
+        $amount = $order->getAmountTotal();
+        $customerId = $order->getOrderCustomer()->getId();
+        $currency = $salesChannelContext->getCurrency()->getIsoCode();
+
+
+        $transactionDate = [
+            'amount' =>  $amount,
+            'currency' => $currency,
+            
+
+        ]
     }
 }
